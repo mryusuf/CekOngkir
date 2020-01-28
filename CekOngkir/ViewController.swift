da062abf2fb3c3a66e2e44fc721475b0b1bc72ca
Muhamad Yusuf Indra <muhamadyusufindra@gmail.com>
2020-01-28 18:35:27 +0700

    Initial Commit

---
+++ CekOngkir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CekOngkir
+//
+//  Created by Indra Permana on 28/01/20.
+//  Copyright © 2020 Yusuf Indra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
